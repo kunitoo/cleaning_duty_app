8c577158d7fbf827fc37f2cf4f89908ab945f505
Kunihiko Ito <kuni-ito@esm.co.jp>
2011-11-17 10:22:24 +0900

    rails new cleaning_duty_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleaningDutyApp::Application.config.secret_token = '3d649adf36621175a00dc557c83dd3f056b68231ea1d07e1ca7b12394549d58bcfaa82fcd21aed19a409881e1093c843bf4e6561260bb990ff1758b908b31181'
